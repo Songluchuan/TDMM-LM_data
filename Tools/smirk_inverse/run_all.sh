ff66fcdfa2039b212d2ee4b0f734d19347131660
YYN <42260891+Songluchuan@users.noreply.github.com>
2026-04-27 00:58:21 -0400

    Delete Tools directory

--- Tools/smirk_inverse/run_all.sh
+++
@@ -1,8 +0,0 @@
-# chmod +x run_all.sh
-
-# 
-./run_samples.sh \
-  /data/haiyang/wan22_datas/HuMo/results/prompt_17_3000 \
-  /data/haiyang/wan22_datas/HuMo/results/prompt_18_3000 \
-  /data/haiyang/wan22_datas/HuMo/results/prompt_19_3000 \
-  /data/haiyang/wan22_datas/HuMo/results/prompt_20_3000 
